db931f8e6511a4cf021855706c946500030a9e95
Andrey Veselovsky <142045012+FunnyLovsky@users.noreply.github.com>
2024-11-24 14:57:41 +0300

    fix: add vercel.json, public dir

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 })
 
 let messages= [];
